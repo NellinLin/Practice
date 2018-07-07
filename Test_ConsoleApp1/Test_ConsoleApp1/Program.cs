75334def33b7963874487ca6063b5a54d6d2248e
NellinLin <ADshishova@mail.ru>
2018-07-07 21:10:21 +0300

    Task1

---
+++ Test_ConsoleApp1/Test_ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test_ConsoleApp1
+{
+    class Conkat
+    {
+      private List<string> elem=new List<string>();
+
+      public void Add(string a)
+        {
+            elem.Add(a);
+        }
+      public void print_el()
+        {
+            foreach (string item in elem)
+                Console.WriteLine(item);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> m_str = new List<string>() { "aaa", "bbb", "ccc" };
+            Conkat con = new Conkat();
+       
+
+            for (int i=0; i<3; i++)
+            {
+                for(int k=0; k<3; k++)
+                {
+                    if (i != k)
+                    {
+                        con.Add(m_str[i] + m_str[k]);
+                    }
+                }
+            }
+            con.print_el();
+            //Console.ReadKey();
+        }
+    }
+}
